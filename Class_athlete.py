4d8ce3519bc8da8576c7f77d76335b510b6d298e
fonslucens <hyunii605@naver.com>
2021-08-25 17:54:22 +0900

    commit

---
+++ Class_athlete.py
@@ -0,0 +1,15 @@
+class Athlete:
+    def __init__(self, value='Jane'):
+        self.inner_value = value
+        print(self.inner_value)
+
+    def getInnerValue(self):
+        return self.inner_value
+
+class Temp(Athlete):
+    def __init__(self):
+        super()
+
+athlete = Athlete(value='fonslucens')   # == Athlete.__init__()
+
+print(athlete.getInnerValue())
